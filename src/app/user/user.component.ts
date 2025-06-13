9733523e793ab8683b757720301ee1b508139f4b
unknown <costache.georgesilviu@yahoo.ro>
2025-06-13 18:39:31 +0300

    rendering dynamic user content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  user = DUMMY_USERS[RANDOM_INDEX];
 }
