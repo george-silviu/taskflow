292a7d7a003d8dc0beeb25a0187417d9312af9c8
silviucostache <costache.georgesilviu@yahoo.ro>
2025-06-18 09:52:23 +0300

    passing users to component using inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,15 +7,14 @@ const RANDOM_INDEX = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[RANDOM_INDEX]); //this variable can be accessed in template
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar) //computing values using signals
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
- 
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
