717151327dd1d467530ac8ebb2445cb8cdd49df6
silviucostache <costache.georgesilviu@yahoo.ro>
2025-06-30 17:23:08 +0300

    started to implement tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent], //add here the components to be called by app - check template
+  imports: [HeaderComponent, UserComponent, TasksComponent], //add here the components to be called by app - check template
 })
 export class AppComponent {
   users = DUMMY_USERS;
 
-  onSelectUser(id: string){
-    console.log('User ID is ', id);
+  selectedUser: any;
+
+  onSelectUser(id: string) {
+    this.selectedUser = DUMMY_USERS.find(user => user.id === id)
   }
 
 }
