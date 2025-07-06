c1d21c5b405c26645a7a58aeb8615f8818dbccb5
unknown <costache.georgesilviu@yahoo.ro>
2025-07-06 15:21:05 +0300

    implementing complete task feature

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
 
 @Component({
@@ -10,6 +10,9 @@ import { type Task } from './task.model';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  @Output() completeTask = new EventEmitter<string>();
 
-  completeTask() {}
+  onTaskComplete(taskId: string) {
+    this.completeTask.emit(taskId);
+  }
 }
