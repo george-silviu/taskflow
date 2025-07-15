3a6ed5441028b386e00b943ad23f3cd0030ccff4
unknown <costache.georgesilviu@yahoo.ro>
2025-07-15 19:54:59 +0300

    persisting tasks in browser local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -8,6 +8,13 @@ import { type NewTaskData } from './task/task.model';
 export class TasksService {
   private tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => userId === task.userId);
   }
@@ -20,9 +27,15 @@ export class TasksService {
       summary: newTaskData.summary,
       dueDate: newTaskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
