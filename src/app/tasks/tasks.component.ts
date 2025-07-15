9e07890cf9fb93c7fc2f0dfd98d5d97079da7b0f
silviucostache <costache.georgesilviu@yahoo.ro>
2025-07-15 16:40:06 +0300

    refactor: move  tasks related logic in a service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-import { DUMMY_TASKS } from './dummy-tasks';
-
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,32 +15,23 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isNewTaskDialogOpen = false;
-  tasks = DUMMY_TASKS;
+
+  constructor(private tasksService: TasksService) {
+  }
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => this.userId === task.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onTaskComplete(taskId: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.tasksService.deleteTask(taskId);
   }
 
-  onNewTaskCreate() {
+  onTaskCreate() {
     this.isNewTaskDialogOpen = true;
   }
 
-  onNewTaskCancel() {
+  onCloseTask() {
     this.isNewTaskDialogOpen = false;
-  }
-
-  onNewTaskAdd(newTaskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: newTaskData.title,
-      summary: newTaskData.summary,
-      dueDate: newTaskData.date
-    })
-     this.isNewTaskDialogOpen = false;
   }
 }
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { DUMMY_TASKS } from "./dummy-tasks";
+import { type NewTaskData } from "./task/task.model";
+
+@Injectable({
+    providedIn: "root"
+})
+export class TasksService {
+    private tasks = DUMMY_TASKS;
+
+    getUserTasks(userId: string) {
+       return  this.tasks.filter((task) => userId === task.userId);
+    }
+
+    addTask(newTaskData: NewTaskData, userId: string) {
+        this.tasks.unshift({
+            id: new Date().getTime().toString(),
+            userId: userId,
+            title: newTaskData.title,
+            summary: newTaskData.summary,
+            dueDate: newTaskData.date
+        })
+    }
+
+    deleteTask(taskId: string) {
+        this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    }
+}
